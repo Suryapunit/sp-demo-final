bbedd264592322ede460ad0a9ac13f3c9d530759
Suryapunith <surya.chithaluri@techconsulting.tech>
2025-04-08 20:35:23 -0400

    test 4

--- lambda_function/lambda_function.py
+++ lambda_function/lambda_function.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     # TODO implement
     print("Hello,World!")
     print("What's up?")
-    print("What's up?")
+    print("What's up????")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
